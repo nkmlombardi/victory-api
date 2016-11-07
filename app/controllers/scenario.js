0d43c9814ae0294e6e68c2938f0401cf404e7f00
nkmlombardi <nkmlombardi@gmail.com>
2016-11-07 00:12:34 -0500

    Scenario create endpoint

--- app/controllers/scenario.js
+++ app/controllers/scenario.js
@@ -2,57 +2,56 @@ module.exports = {
     getSelfAll: function(req, res, next) {
         req.models.Scenario.findAll({
             where: { user_id: req.user.id }
+
         }).then(function(data) {
             res.json({
                 status: req.status.success,
                 data: data
-            });
-        });
+            })
+        })
     },
 
     getSelfAllWithBudgets: function(req, res, next) {
         req.models.Scenario.findAll({
             where: { user_id: req.user.id },
-
             include: {
                 model: req.models.Budget,
                 as: 'budgets',
                 where: { user_id: req.user.id }
             }
+
         }).then(function(data) {
             res.json({
                 status: req.status.success,
                 data: data
-            });
-        });
+            })
+        })
     },
 
     getSelfAllWithCategory: function(req, res, next) {
         req.models.Scenario.findAll({
             where: { user_id: req.user.id },
-
             include: {
                 model: req.models.Budget,
                 as: 'budgets',
                 where: { user_id: req.user.id },
-
                 include: {
                     model: req.models.Category,
                     as: 'category'
                 }
             }
+
         }).then(function(data) {
             res.json({
                 status: req.status.success,
                 data: data
-            });
-        });
+            })
+        })
     },
 
     getSelfAllWithTransactions: function(req, res, next) {
         req.models.Scenario.findAll({
             where: { user_id: req.user.id },
-
             include: {
                 model: req.models.Budget,
                 as: 'budgets',
@@ -69,11 +68,25 @@ module.exports = {
                     }
                 }
             }
+
         }).then(function(data) {
             res.json({
                 status: req.status.success,
                 data: data
-            });
-        });
+            })
+        })
+    },
+
+    postSelf: function(req, res, next) {
+        req.models.Scenario.create({
+            user_id: req.user.id,
+            name: req.body.name
+
+        }).then(function(scenario) {
+            return res.json({
+                status: req.status.success,
+                data: scenario
+            })
+        })
     }
-};
+}
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
-var settings = require('../config')().settings;
-var controllers = require('../app/controllers');
-var cache = require('apicache').options(settings.cache).middleware;
+var settings = require('../config')().settings
+var controllers = require('../app/controllers')
+var cache = require('apicache').options(settings.cache).middleware
 
 
 /*
@@ -11,72 +11,78 @@ module.exports = function(app) {
 
     /* Base Endpoint */
     app.route('/')
-        .get(function(req, res, next) { res.send('ok'); })
-        .post(function(req, res, next) { res.json(req.body); });
+        .get(function(req, res, next) {
+            res.send('ok')
+        })
+        .post(function(req, res, next) {
+            res.json(req.body)
+        })
 
 
     /* Auth Token Resource */
     app.route('/v1/authenticate')
-        .post(controllers.auth.isCredential, controllers.token.postSelfToken);
+        .post(controllers.auth.isCredential, controllers.token.postSelfToken)
 
 
     /* Users Endpoint */
     app.route('/v1/users')
-        .post(controllers.user.postUser);
+        .post(controllers.user.postUser)
 
 
     /* Transactions Resource */
     app.route('/v1/transactions/self')
-        .get(controllers.auth.isBearer, controllers.transaction.getSelfAll);
+        .get(controllers.auth.isBearer, controllers.transaction.getSelfAll)
     app.route('/v1/transactions/self/all')
-        .get(controllers.auth.isBearer, controllers.transaction.getSelfAllWithAll);
+        .get(controllers.auth.isBearer, controllers.transaction.getSelfAllWithAll)
     app.route('/v1/transactions/self/accounts')
-        .get(controllers.auth.isBearer, controllers.transaction.getSelfAllWithAccounts);
+        .get(controllers.auth.isBearer, controllers.transaction.getSelfAllWithAccounts)
     app.route('/v1/transactions/self/plaid')
-        .post(controllers.auth.isBearer, controllers.transaction.postPlaidTransactions);
+        .post(controllers.auth.isBearer, controllers.transaction.postPlaidTransactions)
 
 
     /* Accounts Resource */
     app.route('/v1/accounts/self')
-        .get(controllers.auth.isBearer, controllers.account.getSelfAll);
+        .get(controllers.auth.isBearer, controllers.account.getSelfAll)
     app.route('/v1/accounts/self/transactions')
-        .get(controllers.auth.isBearer, controllers.account.getSelfAllWithTransactions);
+        .get(controllers.auth.isBearer, controllers.account.getSelfAllWithTransactions)
     app.route('/v1/accounts/self/plaid')
-        .post(controllers.auth.isBearer, controllers.account.postPlaidAccounts);
+        .post(controllers.auth.isBearer, controllers.account.postPlaidAccounts)
 
 
     /* Plaid Services */
     app.route('/v1/plaid/connect')
-        .post(controllers.auth.isBearer, controllers.plaid.postConnect);
+        .post(controllers.auth.isBearer, controllers.plaid.postConnect)
     app.route('/v1/plaid/exchange')
-        .post(controllers.auth.isBearer, controllers.plaid.postExchange);
+        .post(controllers.auth.isBearer, controllers.plaid.postExchange)
     app.route('/v1/plaid/webhook/:id')
-        .post(/*   Unauthenticated    */ controllers.plaid.postWebhook);
+        .post( /*   Unauthenticated    */ controllers.plaid.postWebhook)
 
 
     /* Category Resource */
     app.route('/v1/categories/')
-        .get(controllers.auth.isBearer, controllers.category.getAll);
+        .get(controllers.auth.isBearer, controllers.category.getAll)
     app.route('/v1/categories/primary')
-        .get(controllers.auth.isBearer, controllers.category.getAllPrimary);
+        .get(controllers.auth.isBearer, controllers.category.getAllPrimary)
     app.route('/v1/categories/self/transactions')
-        .get(controllers.auth.isBearer, controllers.category.getAllWithTransactions);
+        .get(controllers.auth.isBearer, controllers.category.getAllWithTransactions)
 
 
     /* Scenario Resource */
     app.route('/v1/scenarios/self')
-        .get(controllers.auth.isBearer, controllers.scenario.getSelfAll);
+        .get(controllers.auth.isBearer, controllers.scenario.getSelfAll)
+        .post(controllers.auth.isBearer, controllers.scenario.postSelf)
     app.route('/v1/scenarios/self/budgets')
-        .get(controllers.auth.isBearer, controllers.scenario.getSelfAllWithBudgets);
+        .get(controllers.auth.isBearer, controllers.scenario.getSelfAllWithBudgets)
     app.route('/v1/scenarios/self/category')
-        .get(controllers.auth.isBearer, controllers.scenario.getSelfAllWithCategory);
+        .get(controllers.auth.isBearer, controllers.scenario.getSelfAllWithCategory)
     app.route('/v1/scenarios/self/transactions')
-        .get(controllers.auth.isBearer, controllers.scenario.getSelfAllWithTransactions);
+        .get(controllers.auth.isBearer, controllers.scenario.getSelfAllWithTransactions)
+
 
     /* Budget Resource */
     app.route('/v1/budgets/self')
-        .get(controllers.auth.isBearer, controllers.budget.getSelfAll);
+        .get(controllers.auth.isBearer, controllers.budget.getSelfAll)
     app.route('/v1/budgets/self/transactions')
-        .get(controllers.auth.isBearer, controllers.budget.getSelfAllWithTransactions);
+        .get(controllers.auth.isBearer, controllers.budget.getSelfAllWithTransactions)
 
-};
+}
