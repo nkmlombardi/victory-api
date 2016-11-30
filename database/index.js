61d30190b3fd644cbcc1a2913da5f952df05b959
nkmlombardi <nkmlombardi@gmail.com>
2016-11-30 16:57:08 -0500

    Now able to toggle seeding and force sycing.

--- database/index.js
+++ database/index.js
@@ -3,37 +3,61 @@ var models = require('./models')
 var seeder = require('./seeders')
 
 module.exports = function(settings) {
-    // Create Sequelize database connection
     var database = {
+        /**
+         * We are creating a connection to the database that is utilized by all
+         * endpoints of the API.
+         */
         sequelize: new sequelize(settings.name, settings.user, settings.pass, {
             host: settings.host,
             dialect: settings.type,
             port: settings.port,
             logging: false
-        })
+        }),
+
+        /**
+         * When we force sync, the database rebuilds all of it's tables (models)
+         * as well as their associations. Any schema changes as well as data
+         * inserted into the database is lost when this is done. This should
+         * only ever be done in a development environment.
+         */
+        doSync: (
+            process.env.FORCE_SYNC_DATABASE === 'true' &&
+            process.env.NODE_ENV === 'development'
+        ),
+
+        /**
+         * When we seed the database, we are inserting demonstration data that
+         * located in the /seeders/data directory. In order to seed the database
+         * it is required that it has been force synced and wiped clean,
+         * otherwise there will be injection collisions on the data. This is
+         * intended to be run in a development environment, not on production.
+         */
+        doSeed: (
+            process.env.FORCE_SYNC_DATABASE === 'true' &&
+            process.env.SEED_DATABASE === 'true' &&
+            process.env.NODE_ENV === 'development'
+        )
     }
 
-    // If we are in a development environment and FORCE_SYNC_DATABASE is set to true, then force sync
-    // We don't want to be seeding or force syncing in a production environment, like ever. That's bad.
-    var forceSyncSeed = process.env.FORCE_SYNC_DATABASE === 'true' && process.env.NODE_ENV === 'development'
-
-    console.log('forceSyncSeed: '.blue, forceSyncSeed)
+    console.log('Force Sync: '.blue, database.doSync)
+    console.log('Seed: '.blue, database.doSeed)
 
     // Import models(tables) into database
     models(database)
 
     // Sync models / migrations / seeds to database
-    database.sequelize.sync({ force: forceSyncSeed }).then(function() {
-        console.log('Models ' + (forceSyncSeed ? 'force '.red : '') + 'synced'.green + ' to database.')
+    database.sequelize.sync({ force: database.doSync }).then(function() {
 
         // Seed database if in development mode && FORCE_SYNC_DATABASE set to true
         // We don't want to be seeding in a production environment
-        if (forceSyncSeed) {
+        if (database.doSeed) {
             seeder.down(database)
             seeder.up(database)
-
-            console.log('Database Seeded.')
         }
+
+
+        console.log('Models ' + (database.doSync ? 'force synced'.red : 'soft synced'.green) + ' and' + (database.doSeed ? ' seeded'.red : ' not seeded'.green) + ' to database.')
     }).catch(function(error) {
         console.log('Failed to sync to database: '.red, error)
     })
