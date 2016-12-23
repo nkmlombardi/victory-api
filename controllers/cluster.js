1b3f3a5c8b637c0a85712d8dd63efc498a4de6fe
Raker <nkmlombardi@gmail.com>
2016-12-23 15:50:53 -0500

    Cluster endpoint fix

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -2,10 +2,11 @@ var treebuilder = require('../services/treebuilder');
 var Promise = require("bluebird");
 
 module.exports = {
-    getClusters: function(req, res, next) {
-        req.models.cluster.findAll().then(function(clusters) {
-            return res.json(clusters);
-        });
+    getClusters: async function(req, res, next) {
+        return res.json({
+            status: req.status.success,
+            data: await req.connection.query(`SELECT * FROM BB_ONELINK_CLUSTER`)
+        })
     },
 
     getClustersTree: function(req, res, next) {
