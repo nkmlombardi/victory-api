bede68f5a6651a1f9205e067267d430ef11f04cf
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2017-01-10 10:46:40 -0500

    Specific health endpoint

--- controllers/origin.js
+++ controllers/origin.js
@@ -30,5 +30,12 @@ module.exports = {
                 `SELECT origin_id AS id, statistic_health_score AS health, health_dtm AS timestamp FROM BB_PROJECT_ORIGIN_HEALTH`
             )
         })
+    },
+
+    getOriginHealth: async function(req, res, next) {
+        return res.json({
+            status: req.status.success,
+            data: await req.connection.query(`SELECT * FROM BB_PROJECT_ORIGIN WHERE origin_id = ${req.params.id}`)
+        })
     }
 }
