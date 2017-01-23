16cf649b982a25bd9f0f60bff3e164e862b8efe5
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2017-01-23 14:54:25 -0500

    Fixed issue where git diffs were commited to version control, breakiing the API.

--- controllers/origin.js
+++ controllers/origin.js
@@ -26,30 +26,29 @@ module.exports = {
     getOriginHealth: async function(req, res, next) {
         return res.json({
             status: req.status.success,
-            data: await req.connection.query(
-<<<<<<< HEAD
-                `SELECT ` +
-                    `origin_id                  AS id, ` +
-                    `statistic_health_score     AS health, ` +
-                    `health_dtm                 AS timestamp ` +
-=======
-                `SELECT     origin_id, statistic_health_score AS health_score, health_dtm ` +
->>>>>>> 0a4a985118c0f2f2f52f601c8aa3561adb8254b5
-                `FROM       BB_PROJECT_ORIGIN_HEALTH ` +
-                `WHERE      origin_id = ${req.params.id} ` +
-                `ORDER BY   health_dtm DESC`
-            )
+            data: await req.connection.query(`
+                SELECT
+                    origin_id                   AS id,
+                    statistic_health_score      AS health,
+                    health_dtm                  AS timestamp
+                FROM        BB_PROJECT_ORIGIN_HEALTH
+                WHERE       origin_id = ${req.params.id}
+                ORDER BY    health_dtm DESC
+            `)
         })
     },
 
     getOriginAllHealth: async function(req, res, next) {
         return res.json({
             status: req.status.success,
-            data: await req.connection.query(
-                `SELECT DISTINCT origin_id, AVG(statistic_health_score) AS health_percent, MAX(health_dtm) as health_dtm ` +
-                `FROM BB_PROJECT_ORIGIN_HEALTH ` +
-                `GROUP BY origin_id`
-            )
+            data: await req.connection.query(`
+                SELECT
+                    DISTINCT origin_id,
+                    AVG(statistic_health_score)     AS health_percent,
+                    MAX(health_dtm)                 AS health_dtm
+                FROM        BB_PROJECT_ORIGIN_HEALTH
+                GROUP BY    origin_id
+            `)
         })
     }
 }
