3cd2f79f1f2ef571e23213646fa4e9233b580e43
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2017-01-30 12:04:54 -0500

    Potential dc fixes

--- controllers/datacenter.js
+++ controllers/datacenter.js
@@ -18,6 +18,7 @@ module.exports = {
 
     getDatacenter: async function(request, response, next) {
         if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
+
         try {
             response.query = await request.connection.query(`SELECT * FROM BB_DATA_CENTER WHERE data_center_code = '${request.params.id}'`)
         } catch(error) {
@@ -32,117 +33,11 @@ module.exports = {
         })
     },
 
-    getDatacenterClients: async function(request, response, next) {
-        if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
-        try {
-            response.query = await request.connection.query(`SELECT * FROM BB_CLIENT WHERE client_id IN (` +
-                            `SELECT client_id FROM BB_PROJECT WHERE project_id IN (` +
-                                `SELECT project_id FROM BB_PROJECT_ORIGIN WHERE origin_id IN (` +
-                                    `SELECT origin_id FROM BB_PROJECT_TARGET WHERE cluster_name IN (` +
-                                        `SELECT cluster_name FROM BB_ONELINK_CLUSTER WHERE data_center = '${request.params.id}'` +
-                                    `)` +
-                                `)` +
-                            `)` +
-                        `)`
-                    )
-        } catch(error) {
-            return response.errorHandler(error, request, response)
-        }
-
-        if (response.query.length === 0) return response.errorHandler(1000, request, response)
-
-        response.json({
-            status: request.status['OK'],
-            data: response.query
-        })
-    },
-
-    getDatacenterProjects: async function(request, response, next) {
-        if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
-        try {
-            response.query = await request.connection.query( `SELECT * FROM BB_PROJECT WHERE project_id IN (` +
-                            `SELECT project_id FROM BB_PROJECT_ORIGIN WHERE origin_id IN (` +
-                                `SELECT origin_id FROM BB_PROJECT_TARGET WHERE cluster_name IN (` +
-                                    `SELECT cluster_name FROM BB_ONELINK_CLUSTER WHERE data_center = '${request.params.id}'` +
-                                `)` +
-                            `)` +
-                        `)`
-                    )
-        } catch(error) {
-            return response.errorHandler(error, request, response)
-        }
-
-        if (response.query.length === 0) return response.errorHandler(1000, request, response)
-
-        response.json({
-            status: request.status['OK'],
-            data: response.query
-        })
-    },
-
-    getDatacenterOrigins: async function(request, response, next) {
-        if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
-        try {
-            response.query = await request.connection.query( `SELECT * FROM BB_PROJECT_ORIGIN WHERE origin_id IN (` +
-                            `SELECT origin_id FROM BB_PROJECT_TARGET WHERE cluster_name IN (` +
-                                `SELECT cluster_name FROM BB_ONELINK_CLUSTER WHERE data_center = '${request.params.id}'` +
-                            `)` +
-                        `)`
-                    )
-        } catch(error) {
-            return response.errorHandler(error, request, response)
-        }
-
-        if (response.query.length === 0) return response.errorHandler(1000, request, response)
-
-        response.json({
-            status: request.status['OK'],
-            data: response.query
-        })
-    },
-
-    getDatacenterTargets: async function(request, response, next) {
-        if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
-        try {
-            response.query = await request.connection.query(`SELECT * FROM BB_PROJECT_TARGET WHERE cluster_name IN (` +
-                            `SELECT cluster_name FROM BB_ONELINK_CLUSTER WHERE data_center = '${request.params.id}'` +
-                        `)`
-                    )
-        } catch(error) {
-            return response.errorHandler(error, request, response)
-        }
-
-        if (response.query.length === 0) return response.errorHandler(1000, request, response)
-
-        response.json({
-            status: request.status['OK'],
-            data: response.query
-        })
-    },
-
-    getDatacenterServers: async function(request, response, next) {
-        if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
-        try {
-            response.query = await request.connection.query(`SELECT * FROM BB_ONELINK_SERVER WHERE cluster_name IN (` +
-                            `SELECT cluster_name FROM BB_ONELINK_CLUSTER WHERE data_center = '${request.params.id}'` +
-                        `)`
-                    )
-        } catch(error) {
-            return response.errorHandler(error, request, response)
-        }
-
-        if (response.query.length === 0) return response.errorHandler(1000, request, response)
-
-        response.json({
-            status: request.status['OK'],
-            data: response.query
-        })
-    },
-
     getDatacenterClusters: async function(request, response, next) {
         if (utility.isUppercaseDashColon(request.params.id) === false) return response.errorHandler(1001, request, response)
+
         try {
-            response.query = await request.connection.query(`SELECT * FROM BB_ONELINK_CLUSTER WHERE data_center = '${request.params.id}'`)
+            response.query = await request.connection.query(`SELECT * FROM BB_ONELINK_CLUSTER WHERE data_center_code = '${request.params.id}'`)
         } catch(error) {
             return response.errorHandler(error, request, response)
         }
