5e96e46a1c7d350f12d58a97ffde73981fa7e6a7
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2017-02-23 14:21:28 -0500

    Fixes to JWT error handling w/ Josh

--- services/authentication/jwt.strategy.js
+++ services/authentication/jwt.strategy.js
@@ -6,6 +6,7 @@ const passport = require('passport')
 const jwt = require('jsonwebtoken')
 const moment = require('moment')
 const handlers = require('../handlers')
+const utility = require('../utilities')
 
 passport.use(new Strategy({
     secretOrKey: process.env.API_SECRET,
@@ -18,14 +19,18 @@ passport.use(new Strategy({
 
         // Verify the JWT appears in auth, is a legitimate token, and hasn't been manipulated
         jwt.verify(jwt_auth_token, process.env.API_SECRET, async (error, decoded) => {
-            if (request.client_ip_addr !== decoded.user_ip) return callback(null, null, Error('4006'))
+            if (request.client_ip_addr !== decoded.user_ip) return callback(null, null, new ApiError(4006))
             token = await request.models.Passport.findOne({ where: { jwt_token: jwt_auth_token } })
-            if (!token) return callback(null, null, Error('5002'))
-            if (token.deleted_at) return callback(null, null, Error('4005'))
+
+            if (!token) return callback(null, null, new ApiError(5002))
+
+            if (token.deleted_at) return callback(null, null, new ApiError(4005))
+
             if (!moment(moment(token.updated_at).format()).isAfter(moment().subtract(6, 'hours').format())) {
                 token.deleted_at = moment().format()
-                return callback(null, null, Error('4005'))
+                return callback(null, null, new ApiError(4005))
             }
+
             // If all is well, return the payload
             return callback(null, decoded, false)
         })
@@ -34,13 +39,16 @@ passport.use(new Strategy({
 
 module.exports = function (request, response, next) {
     passport.authenticate('jwt', (info, user, error) => {
-        // If we have an error, check and see if the message is an error code,
-        // if so, throw that custom error, if not, throw default catch all error
-        if (error) {
-            if (!isNaN(error.message)) error.message = Number(error.message)
-            console.log('error loop, error: ', error)
-            return handlers.error(new ApiError(error.message))
+        // Check to see if it's an error we returned
+        if (error instanceof ApiError) {
+            return handlers.error((error.code || error), (status, payload) => response.status(status).json(payload))
         }
+
+        // Check to see if it's an error the passport library returned
+        if (error instanceof Error) {
+            return handlers.error((error.message || error), (status, payload) => response.status(status).json(payload))
+        }
+
         token.changed('updated_at', true).save()
 
         return next()
